04ae396889ca0b9d94c59a8e46676b3698be8927
unknown <geraud.bernonville@PAR-LAP-639.valtech.com>
2012-01-06 14:33:44 +0100

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hellorails::Application.config.secret_token = 'ea2799ba6ba9995ef4d794ab1eade3798d3d143c9449031fb11286098adcf678e84013c1f0a38fdc8c3383ca9da4994e7bfbf8c9651b2b05db06c9e55f575b8e'
