6b35c0335e483932342585a8b35b48f2d04cf2d7
unknown <geraud.bernonville@PAR-LAP-639.valtech.com>
2012-01-06 15:53:44 +0100

    Security added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  http_basic_authenticate_with :name => "dhh", :password => "secret", :except => [:index, :show]
+
   # GET /posts
   # GET /posts.json
   def index
