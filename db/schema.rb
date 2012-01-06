bc3b5ab2c9b82d418eb48afbc13fc2ecfd847249
geraudster <geraudster@gmail.com>
2012-01-06 16:45:50 +0100

    add tags (1st part)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120106140005) do
+ActiveRecord::Schema.define(:version => 20120106145443) do
 
   create_table "comments", :force => true do |t|
     t.string   "commenter"
@@ -30,5 +30,14 @@ ActiveRecord::Schema.define(:version => 20120106140005) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "tags", :force => true do |t|
+    t.string   "name"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "tags", ["post_id"], :name => "index_tags_on_post_id"
 
 end
